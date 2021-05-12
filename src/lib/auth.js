7caa1d1bf9a487715e80d809bed1f0df3925de06
THINKPAD\Mateo <mposada@lean-tech.io>
2021-05-12 11:04:30 -0500

    protecting routes from the server

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -5,6 +5,13 @@ module.exports = {
             return next();
         }
         return res.redirect('/signin');
+    },
+
+    isNotLoggedIn(req, res, next) {
+        if (!req.isAuthenticated()) {
+            return next();
+        }
+        return res.redirect('/profile');
     }
 
 };
