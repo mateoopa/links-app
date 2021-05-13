9d1fcad60be685ea377363e53295a4288b385aa9
THINKPAD\Mateo <mposada@lean-tech.io>
2021-05-12 19:17:53 -0500

    Test

--- src/keys.js
+++ src/keys.js
@@ -2,9 +2,9 @@ module.exports = {
 
     database: {
         host: 'us-cdbr-east-03.cleardb.com',
-        user: 'b64d6a968f8397',
-        password: '75005851',
+        user: 'b5a2298141ab10',
+        password: '819610f8',
         port: 3306,
-        database: 'heroku_ee047d3ece154f6'
+        database: 'heroku_5ad4c2af1d62c51'
     }
 }
